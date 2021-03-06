f99e3a843b7ce15fead0c1bbfe7ab7eeec31ce15
Osiclin <67206380+Osiclin@users.noreply.github.com>
2021-03-06 13:11:20 +0100

    responsiveness 1

--- components/Group60.js
+++ components/Group60.js
@@ -5,16 +5,16 @@ export default function Group60() {
     const story = `I had the best experience shopping with vasiti. Usability of the website was great, very good customer service, an all round great experience. I would definately be coming back! I had the best experience shopping with vasiti. Usability of the website was great, very good customer service, an all round great experience. I would definately be coming back!`
 
     return(
-        <>
         <div className={styles.group60}>
+            <div>
         <svg className={styles.ecllipse8} width="631" height="631" viewBox="0 0 631 631" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="315.5" cy="315.5" r="315.5" fill="#2E2E2E"/>
         </svg>
 
             <div className={styles.testimonialimage2}></div>
+            </div>
+            <Story name={`Tolu & Joy’s Experience`} status={`CUSTOMER`} story={story}/>
         </div>
-        <Story name={`Tolu & Joy’s Experience`} status={`CUSTOMER`} story={story}/>
-        </>
     )
 }
 
--- components/Group61.js
+++ components/Group61.js
@@ -6,16 +6,16 @@ export default function Group60() {
     const story = `I had the best experience shopping with vasiti. Usability of the website was great, very good customer service, an all round great experience. I would definately be coming back! I had the best experience shopping with vasiti. Usability of the website was great, very good customer service, an all round great experience. I would definately be coming back!`
      
     return(
-        <>
         <div className={styles.group61}>
-        <svg  className={styles.ecllipse1} width="631" height="631" viewBox="0 0 631 631" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="315.5" cy="315.5" r="315.5" fill="#FF5C00" fillOpacity="0.03"/>
-        </svg>
+            <Story1 name={`Josiah’s Experience`} status={`VENDOR`} story={story}/>
+            <div>
+                <svg  className={styles.ecllipse1} width="631" height="631" viewBox="0 0 631 631" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="315.5" cy="315.5" r="315.5" fill="#FF5C00" fillOpacity="0.03"/>
+                </svg>
 
-        <div className={styles.testimonialimage1}></div>
-        </div>
-        <Story1 name={`Josiah’s Experience`} status={`VENDOR`} story={story}/>
-        </>
+                <div className={styles.testimonialimage1}></div>
+            </div>
+        </div>        
     )
 }
 
