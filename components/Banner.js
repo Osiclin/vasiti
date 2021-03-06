c1143a90c8df87753217607862eb79fef2ba4909
Osiclin <67206380+Osiclin@users.noreply.github.com>
2021-03-06 21:24:34 +0100

    responsive 4

--- components/Banner.js
+++ components/Banner.js
@@ -9,6 +9,7 @@ export default function Banner() {
         </div>
         <div className={styles.subscribeform}>
             <p className={styles.title}>Be a member <br />of our community 🎉</p>
+            
             <p className={styles.para}>We’d make sure you’re always first to know what’s <br />happening on Vasiti—thus, the world.</p>
             <input className={styles.input} type="email" placeholder="enter your email address"/> <button className={styles.button}>SUBSCRIBE</button>
         </div>
